8d7f5963a20fc72f12dc913a27efd4113ded88cb
oluwatobi oyejide <moyejide2016@gmail.com>
2020-09-10 21:31:38 +0100

    added server communication

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import PhoneBookFilter from "./components/PhoneBookFilter";
 import PhoneBookForm from "./components/PhoneBookForm";
 import Contacts from "./components/Contacts";
 
 function App() {
-  let [contacts, setContacts] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  let [contacts, setContacts] = useState([]);
+  //   [
+  //   { name: "Arto Hellas", number: "040-123456" },
+  //   { name: "Ada Lovelace", number: "39-44-5323523" },
+  //   { name: "Dan Abramov", number: "12-43-234345" },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122" },
+  // ]
   let [contact, setContact] = useState({ name: "", number: "" });
   let [filterValue, setFilterValue] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setContacts(response.data);
+    });
+  }, []);
 
   const getContactsToShow = () => {
     if (filterValue === "") return contacts;
     let formatedFilterValue = filterValue.toUpperCase();
     return contacts.filter((contact) => {
-      return contact.name.toUpperCase().includes(formatedFilterValue);
+      return (
+        contact.name.toUpperCase().includes(formatedFilterValue) ||
+        contact.number.includes(formatedFilterValue)
+      );
     });
   };
 
