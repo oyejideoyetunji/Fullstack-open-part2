b134d99852045c0dcdfeaf3f33bbeb7a8e6e1d2e
oluwatobi oyejide <moyejide2016@gmail.com>
2020-11-12 06:54:31 +0100

    coupled to a live server

--- phone-book/src/services/persons.js
+++ phone-book/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 export const getAll = () => axios.get(baseUrl);
 
@@ -10,3 +10,10 @@ export const update = (id, newObject) =>
   axios.put(`${baseUrl}/${id}`, newObject);
 
 export const deleteObject = (id) => axios.delete(`${baseUrl}/${id}`);
+
+
+/**
+ * json server url http://localhost:3001/persons
+ * 
+ * live server base url https://phonenote.herokuapp.com
+ */
